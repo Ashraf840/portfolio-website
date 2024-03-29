38af0c39111d7150d062e213e3b2961e9d127b10
Ashraf840 <shift2unleashed2011@gmail.com>
2024-03-30 00:52:09 +0600

    Rephrase the intro paragraph & added a section divider; Also added the staggered animation into that

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/src/components/intro";
+import SectionDivider from "@/src/components/section-divider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center px-4">
       <Intro />
+      <SectionDivider />
     </main>
   );
 }
--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -35,15 +35,14 @@ export default function Intro() {
                 </div>
             </div>
 
-            <motion.p className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+            <motion.h1 className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
                 initial={{ opacity: 0, y: 100 }}
                 animate={{ opacity: 1, y: 0 }}
             >
                 <span className="font-bold">Hello, I'm Kh Tanjim Ashraf.</span> I'm a{" "}
                 <span className="font-bold">software engineer</span> with{" "}
-                <span className="font-bold">4 years</span> of experience. I enjoy
-                building <span className="italic">web apps</span>.
-            </motion.p>
+                <span className="font-bold">4 years</span> of experience. I engineer web apps with <span className="italic">real-world significance.</span>
+            </motion.h1>
 
             <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
                 initial={{ opacity: 0, y: 100 }}

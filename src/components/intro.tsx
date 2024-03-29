6fc42119b04167290b0ed65b4e0fbdefd46f7e16
Ashraf840 <shift2unleashed2011@gmail.com>
2024-03-29 20:29:14 +0600

    Design, animate & ensure responsiveness of the intro section buttons

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -52,16 +52,16 @@ export default function Intro() {
                     delay: 0.1
                 }}
             >
-                <Link href="#contact" className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full'>
-                    Contact me here <BsArrowRight />{" "}
+                <Link href="#contact" className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>
+                    Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />{" "}
                 </Link>
-                <a className="bg-white px-7 py-3 flex items-center gap-2 rounded-full">
-                    Download Resume <HiDownload />
+                <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" href='/CV.pdf' download>
+                    Download Resume <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
                 </a>
-                <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full'>
+                <a className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10' href='https://www.linkedin.com/' target='_blank'>
                     <BsLinkedin />
                 </a>
-                <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full'>
+                <a className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10' href='https://github.com/' target='_blank'>
                     <FaGithubSquare />
                 </a>
             </motion.div>

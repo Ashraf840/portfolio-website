3be59ac21ff502b6186beb15757d7ed5ab288451
Ashraf840 <shift2unleashed2011@gmail.com>
2024-03-31 11:04:19 +0600

    Refator the useInView() hook by separating into hooks.ts file complying the DRY principle - ActiveSectionTracking

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,24 +1,14 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import SectionHeading from '@/src/components/section-heading'
 import { projectsData } from '@/lib/data'
 import Project from '@/src/components/project'
-import { useActiveSectionContext } from '@/src/context/active-section-context'
-import { useInView } from 'react-intersection-observer'
+import { useSectionInView } from '@/lib/hooks'
 
 
 export default function Projects() {
-    const { ref, inView } = useInView({
-        threshold: 0.5,
-    });
-    const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
-
-    useEffect(() => {
-        if (inView && Date.now() - timeOfLastClick > 1000) {
-            setActiveSection("Projects")
-        }
-    }, [inView, setActiveSection, timeOfLastClick]);
+    const { ref } = useSectionInView('Projects', 0.5);
 
     return (
         <section ref={ref} id='projects' className='scroll-mt-28'>

c64505861f609d82214af112ee4a111f7b566615
Ashraf840 <shift2unleashed2011@gmail.com>
2024-03-31 19:57:32 +0600

    Added general styling for the experience section

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -12,7 +12,11 @@ export default function Experience() {
     const { ref } = useSectionInView('Experience');
 
     return (
-        <section ref={ref} id='experience'>
+        <section
+            ref={ref}
+            id='experience'
+            className='mb-28 scroll-mt-28 sm:mb-40'
+        >
             <SectionHeading>
                 My Experience
             </SectionHeading>

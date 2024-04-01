f3d83c22718d87d53a948b7317f3345184cd6dec
Ashraf840 <shift2unleashed2011@gmail.com>
2024-04-02 05:52:49 +0600

    Preserve theme pref using useState hook; including tracking of client's theme pref of OS if not selected any theme pref from the app

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BsMoon, BsSun } from 'react-icons/bs'
 
 type Theme = "light" | "dark";
@@ -11,10 +11,25 @@ export default function ThemeSwitch() {
     const toggleTheme = () => {
         if (theme === "light") {
             setTheme("dark");
+            window.localStorage.setItem("theme", "dark");
         } else {
             setTheme("light");
+            window.localStorage.setItem("theme", "light");
         }
     }
+
+    // When the app requires to synchronize with an external system
+    useEffect(() => {
+        const localTheme = window.localStorage.getItem("theme") as Theme | null;
+
+        // If not null, set theme to the app's setTheme state; Take dominant if user set the theme preference through explicitly setting the button; otherwise check the use's default theme pref from his OS.
+        if (localTheme) {
+            setTheme(localTheme)
+        } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            setTheme("dark");
+        }
+
+    }, []);
 
     return (
         <button className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all'

8c6db5e02a9e860de53a92db8ca66e8b532dee6b
Ashraf840 <shift2unleashed2011@gmail.com>
2024-04-01 16:42:54 +0600

    Added loading spinner animation to submit button while sending email from contact form

--- src/actions/sendEmail.ts
+++ src/actions/sendEmail.ts
@@ -1,7 +1,9 @@
 "use server"
 
+import React from 'react'
 import { Resend } from 'resend';
 import { validateString, getErrorMessage } from '@/lib/utils';
+import ContactFormEmail from '@/email/contact-form-email';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -28,7 +30,11 @@ export const sendEMail = async (formData: FormData) => {
             to: "shift2unleashed2011earn1994@gmail.com",
             subject: "Message from portfolio contact form",
             reply_to: senderEmail as string,
-            text: message as string,
+            // text: message as string,
+            react: React.createElement(ContactFormEmail, {
+                message: message as string,
+                senderEmail: senderEmail as string,
+            })
         });
     } catch (error: unknown) {
         // Gracefully handling error

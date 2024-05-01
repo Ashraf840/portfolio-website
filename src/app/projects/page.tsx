32d64f4a48fc915195635dd5be45c338ad83b5f7
Ashraf840 <shift2unleashed2011@gmail.com>
2024-05-01 12:46:37 +0600

    Created the "Show More" button to route to all the projects page; Added styling to the button ;

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import { projectsData } from '@/lib/data'
 
 export default function Projects() {
     return (
-        <div>My Projects</div>
+        <section>
+            <div>My Projects</div>
+            <div>
+                {
+                    projectsData.map((project, index) => (
+                        <React.Fragment key={index}>
+                            <Project {...project} />
+                        </React.Fragment>
+                    ))
+                }
+            </div>
+        </section>
+    )
+}
+
+type ProjectProps = (typeof projectsData)[number]
+
+function Project({ title, description, tags, imageUrl }: ProjectProps) {
+    return (
+        <section>
+            <h1>{title}</h1>
+            <p>{description}</p>
+            <ul>
+                {
+                    tags.map((tag, index) => (
+                        <li key={index}>{tag}</li>
+                    ))
+                }
+            </ul>
+        </section>
     )
 }

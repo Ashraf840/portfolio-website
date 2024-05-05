eb8226a088947882694483990352691eba4c37a5
Ashraf840 <shift2unleashed2011@gmail.com>
2024-05-05 10:48:06 +0600

    Removed the profile page; Make slight changes to the data.ts file;

--- src/app/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Profile() {
-    return (
-        <div>
-            <h1 className='text-4xl py-10 text-center mb-10'>My Profile</h1>
-        </div>
-    )
-}
